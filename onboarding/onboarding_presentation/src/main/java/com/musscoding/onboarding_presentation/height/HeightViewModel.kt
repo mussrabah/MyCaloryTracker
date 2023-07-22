66b5db4c47d4975478943d2c903f0cc574e685f5
mussrbh <mustrbh@gmail.com>
2023-07-22 01:24:50 +0100

    implemented HeightScreen and WeightScreen

---
+++ onboarding/onboarding_presentation/src/main/java/com/musscoding/onboarding_presentation/height/HeightViewModel.kt
@@ -0,0 +1,57 @@
+package com.musscoding.onboarding_presentation.height
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.musscoding.core.domain.preferences.Preferences
+import com.musscoding.core.domain.use_case.FilterOutDigits
+import com.musscoding.core.util.UiEvent
+import com.musscoding.core.util.UiText
+import com.musscoding.core.R
+import com.musscoding.core.navigation.Route
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeightViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits
+): ViewModel() {
+
+    var height by mutableStateOf("180")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onHeightEnter(height: String) {
+        if (height.length <= 3) {
+            //filter the string because we don't want negative, decimal or bigger than three
+            //digits numbers
+            //but filtering a number is business logic so we don't wanna
+            //do it here instead we'll do it in a separate use case
+            //use cases belong to the domain layer
+            this.height = filterOutDigits(height)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val heightNumber = height.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        UiText.StringResource(R.string.error_height_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveHeight(heightNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.WEIGHT))
+        }
+    }
+}
