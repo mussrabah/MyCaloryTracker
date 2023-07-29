3de5f0f69e187a621f2d7459d57ac8074170cd62
mussrbh <mustrbh@gmail.com>
2023-07-29 19:18:44 +0100

    implemented the ExpandableMeal UI

---
+++ app/src/main/java/com/musscoding/mycalorytracker/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.musscoding.mycalorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.musscoding.core.navigation.Route
+import com.musscoding.mycalorytracker.navigation.navigate
+import com.musscoding.mycalorytracker.ui.theme.MyCaloryTrackerTheme
+import com.musscoding.onboarding_presentation.activity.ActivityScreen
+import com.musscoding.onboarding_presentation.age.AgeScreen
+import com.musscoding.onboarding_presentation.gender.GenderScreen
+import com.musscoding.onboarding_presentation.goal.GoalScreen
+import com.musscoding.onboarding_presentation.height.HeightScreen
+import com.musscoding.onboarding_presentation.nutrient_goal.NutrientGoalScreen
+import com.musscoding.onboarding_presentation.weight.WeightScreen
+import com.musscoding.onboarding_presentation.welcome.WelcomeScreen
+import com.musscoding.tracker_presentation.tracker_overview.TrackerOverviewScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCaloryTrackerTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {padding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME,
+                        modifier = Modifier.padding(padding)
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.HEIGHT) {
+                            HeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.WEIGHT) {
+                            WeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.ACTIVITY) {
+                            ActivityScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GOAL) {
+                            GoalScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
+                            TrackerOverviewScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.SEARCH) {
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
--- tracker/tracker_presentation/src/main/java/com/musscoding/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/musscoding/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -12,6 +13,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.musscoding.core.util.UiEvent
 import com.musscoding.core_ui.LocalSpacing
 import com.musscoding.tracker_presentation.tracker_overview.components.DaySelector
+import com.musscoding.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.musscoding.tracker_presentation.tracker_overview.components.NutrientHeader
 
 @Composable
@@ -44,6 +46,18 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) {meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleMealClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                },
+                content = {
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
